6eb45350c59a1b1756875d9dc5300554660efe69
adityasavaliya01 <168553483+adityasavaliya01@users.noreply.github.com>
2024-04-30 06:17:06 -0700

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,30 @@
+# x={3:'hii', 'hello':5.66, 'demo':'world', 'my':'123'}
+# print(x)
+# y={'class':[1,'it',1.432], (1,'it',9999.0):'1234'}
+# print(y)
+# print(type(x))
+
+
+b={1.232:'hii', 'heello':'22', 'hell':'lleh'}
+# print(b)
+# x=b.copy()
+# print(x)
+
+# b.copy()
+# print(b)
+
+# b.clear()
+# print(b)
+
+# b.update({'heello':'222'})
+# print(b)
+
+# print(b.items())
+# print(b.keys())
+# print(b.values())
+# print(b.get('hell'))
+# print(b.get(1.232))
+# b.pop('heello')
+# print(b)
+# b.popitem()
+# print(b)
