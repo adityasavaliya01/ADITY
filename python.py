91a0ec931c9028af7d6fde11b4baf8f7c801abfb
adityasavaliya01 <168553483+adityasavaliya01@users.noreply.github.com>
2024-04-30 06:19:28 -0700

    Add files via upload

---
+++ python.py
@@ -0,0 +1,25 @@
+# a=2
+# b=2
+# if('a==b'):
+#     print('a is equal to b')
+# else:
+#     print('a is not equal to b')
+
+# x=10
+# y=15
+# if ('x!=y'):
+#     print('x is equal to y')
+# else: 
+#     print ('x is not equal to y')
+
+# hello=['a','b',1.232,'xyz','hi']
+# print(type(hello))
+# print(hello[2])
+# print(hello[1:8])
+# print(hello[-2])
+# print(hello[-2::-1])
+# print(hello[1:-1])
+# print(hello[::-2])
+# print(hello[2:-1])
+
+# list is mutable-we can make changes
