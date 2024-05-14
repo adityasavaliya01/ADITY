be0c05b40c2ae6deb1b069d0d639e944881482e2
adityasavaliya01 <168553483+adityasavaliya01@users.noreply.github.com>
2024-05-14 17:24:24 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,73 @@
+# x=5
+# y=-4
+# print(x+y)
+
+# a=4
+# b=3
+# print(a-b)
+
+# c=10
+# d=-100
+# print(c*d)
+
+# e=6
+# f=12
+# print(e/f)
+
+# a=12
+# b=3
+# print(a%b)
+
+# print(2+4)
+
+# b=3
+# c=-2.0
+# print(b**c)
+
+# a=10
+# b=4
+# print(a//b)
+
+# assignment
+
+# x=5
+# print(x)
+
+# y=0
+# y+=5
+# print(y)
+
+a=10
+a-=11
+print(a)
+
+# comparison
+
+# ==
+# !=
+# x=4
+# y=2
+# if x>y:
+#     print('right')
+# else:
+#     print('wrong')
+
+# if 5<3:
+#     print('wrong')
+# else:
+#     print('right')
+
+# if 3>=2:
+#     print('correct')
+# else:
+#     print('incorrect')
+
+# a=22
+# b=20
+# if a<=b:
+#     print('ruight')
+# else:
+#     print('wrong')
+
+
+
