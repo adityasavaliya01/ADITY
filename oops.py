3659bcb1c36013a0723ee47ce5830d8471135486
adityasavaliya01 <168553483+adityasavaliya01@users.noreply.github.com>
2024-09-24 18:16:19 +0530

    Add files via upload

---
+++ oops.py
@@ -0,0 +1,90 @@
+# oops 
+
+# inheritance
+# class x():
+#     def aditya(self):
+#         print('aditya0')
+#     def aditya2(self):
+#         print('aditya1')
+
+# class y(x):
+#     def aditya3(self):
+#         print('aditya2')
+#     def aditya4(self):
+#         print('aditya3')
+# class z(y):
+#     def aditya5(self):
+#         print('aditya4')
+#     def aditya6(self):
+#         print('aditya8')
+
+# a=z()
+# a.aditya()
+# a.aditya2()
+# a.aditya3()
+# a.aditya4()
+# a.aditya5()
+# a.aditya6()
+
+# polymorfisom
+# class a():
+#     def __init__(self,a,b):
+#         self.ab=a
+#         self.bc=b
+#         print(a)
+#         print('class1')
+#     def g(self):
+#         print('nothing')
+# class b():
+#     def __init__(self,c,d):
+#         self.hg=c
+#         self.nj=d
+#         print(c)
+#         print('class2')
+#     def g(self):
+#         print('nothing')
+# class c():
+#     def __init__(self,e,f):
+#         self.xy=e
+#         self.tu=f
+#         print(e)
+#         print('class3')
+#     def g(self):
+#         print('nothing')
+
+# aditya=a('patel','patil')
+# aditi=b('shri','pattto')
+# sagar=c('trans','tata')
+
+# for x in (aditya,aditi,sagar):
+#     x.g()
+
+# itrater
+
+# x=[1,2,3,'aditya',1.2]
+# y=iter(x)
+# print(next(y))
+# print(next(y))
+# print(next(y))
+# print(next(y))
+# print(next(y))
+
+# math
+# a=pow(2,2)
+# print(a)
+
+# import math
+# x=math.pi
+# print(x)
+
+
+# i='banana'
+# i=0
+# while i<7:
+#     i+=1
+
+x=str(input('sagar dholariya'))
+for y in x:
+    while y<=6:
+        y+=1
+        print(y)
